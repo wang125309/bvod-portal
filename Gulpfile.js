9268370f289f33ff2a484f65ba6f9a0147b449e3
Jarvys <yuhan534@126.com>
2014-07-22 16:53:12 +0800

    scaffold

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,27 @@
 var gulp = require("gulp");
-var less = require("gulp-less");
+var fileinclude = require("gulp-file-include");
+var sass = require("gulp-sass");
 
-gulp.task('less', function() {
+gulp.task('sass', function() {
+    return gulp.src("scss/*.scss")
+        .pipe(sass())
+        .on('error', console.error)
+        .pipe(gulp.dest("dist/css"));
 });
 
+gulp.task('templates', function() {
+    return gulp.src(["html/index.html"])
+        .pipe(fileinclude())
+        .on('error', console.error)
+        .pipe(gulp.dest("dist"));
+});
+
+gulp.task("watch-templates", function() {
+    gulp.watch("html/*.html", ["templates"]);
+});
+
+gulp.task("watch-sass", function() {
+    gulp.watch("scss/*.scss", ["sass"]);
+});
+
+gulp.task("watch", ["watch-sass", "watch-templates"]);
