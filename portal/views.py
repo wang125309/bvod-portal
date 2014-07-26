35ab87fd20071fbec1ea125f8e9d84eb4f4914f8
PopPopRet <lance.through@the.heart>
2014-07-26 17:10:56 +0800

    redesign api

--- portal/views.py
+++ portal/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from query import *
 
 def index(request):
-	featured = fetch_media_list()
-	recently = fetch_media_list(limit=5)
-	popular = fetch_media_list()
-	content = {'featured': featured, 'recently': recently, 'popular': popular}
+	featured = fetch_featured_media()
+	recently = fetch_popular_media()
+	popular = fetch_recently_media()
+	categories = fetch_category_tree()
+	departments = fetch_department_list()
+	ads = fetch_ad_list()
+
+	content = {'featured': featured, 'recently': recently, 'popular': popular, 'categories': categories, 'departments': departments, 'ads': ads}
 
 	return render(request, "index.html", content)
