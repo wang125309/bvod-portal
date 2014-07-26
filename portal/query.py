7a92106f64d4fa254fd253148e571f127d0c4bb4
PopPopRet <lance.through@the.heart>
2014-07-26 16:05:49 +0800

    fake api

--- portal/query.py
+++ portal/query.py
@@ -2,9 +2,9 @@ import requests
 
 base_url = 'http://bvod.limijiaoyin.com/api/'
 
-def fetch_media_list(payload):
+def fetch_media_list(type='video', category='', featured='', popular='', limit=7):
    api_url = base_url+'media'
-   r = requests.get(api_url, params=payload)
+   r = requests.get(api_url, params={'type':type, 'category':category, 'featured': featured, 'popular':popular, 'limit':limit})
    if r.status_code == 200:
       return r.json()
    else:
@@ -18,5 +18,13 @@ def fetch_category_tree(depth=1):
    else:
       return {"count":0, "categories":[]}
 
+def fetch_department_list(limit = 7):
+   departs = {'count': 3*limit, 'departments': [{'id': 5, 'name': 'FFF', 'avatar': 'images/11.png'} for i in range(limit)]}
+   return departs
+
+def fetch_ad_list(limit = 7):
+   ads = {'count': 3*limit, 'departments': [{'id': 1, 'seq': i, 'title': 'OOOOO', 'desc': 'sodfasofnjaofasfddddddddas asdfa sdfasdfasdfasdf asdgfl as gfasdfa dd', 'image': 'images/ad_s.jpg', 'url': 'www.baidu.com'} for i in range(limit)]}
+   return ads
+
 if __name__ == '__main__':
-	print fetch_category_tree(1)
+	print fetch_ad_list()
--- portal/views.py
+++ portal/views.py
@@ -2,13 +2,9 @@ from django.shortcuts import render
 from query import *
 
 def index(request):
-	featured_query = {'type':'video', 'category':'', 'limit':7}
-	recently_query = {'type':'video', 'category':'', 'limit':5}
-	popular_query = {'type':'video', 'category':'', 'limit':7}
-
-	featured = fetch_media_list(featured_query)
-	recently = fetch_media_list(recently_query)
-	popular = fetch_media_list(popular_query)
+	featured = fetch_media_list()
+	recently = fetch_media_list(limit=5)
+	popular = fetch_media_list()
 	content = {'featured': featured, 'recently': recently, 'popular': popular}
 
 	return render(request, "index.html", content)
