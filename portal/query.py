0884806803317c438a3ada736eafb9b4f68ee599
PopPopRet <lance.through@the.heart>
2014-07-26 13:33:50 +0800

    接口调整

--- portal/query.py
+++ portal/query.py
@@ -1,10 +1,22 @@
 import requests
 
-api_url = 'http://bvod.limijiaoyin.com/api/media'
+base_url = 'http://bvod.limijiaoyin.com/api/'
 
 def fetch_media_list(payload):
+   api_url = base_url+'media'
    r = requests.get(api_url, params=payload)
    if r.status_code == 200:
       return r.json()
    else:
       return {"count":0, "media":[]}
+
+def fetch_category_tree(depth=1):
+   api_url = base_url+'categories/tree'
+   r = requests.get(api_url, params={'depth':depth})
+   if r.status_code == 200:
+      return r.json()
+   else:
+      return {"count":0, "categories":[]}
+
+if __name__ == '__main__':
+	print fetch_category_tree(1)
