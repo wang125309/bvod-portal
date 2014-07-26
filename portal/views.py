f9f1c63d6f645635a1f4d9793fa2ad4bdb456c27
Jarvys <yuhan534@126.com>
2014-07-26 17:03:35 +0800

    save

--- portal/views.py
+++ portal/views.py
@@ -5,6 +5,10 @@ def index(request):
 	featured = fetch_media_list()
 	recently = fetch_media_list(limit=5)
 	popular = fetch_media_list()
-	content = {'featured': featured, 'recently': recently, 'popular': popular}
 
-	return render(request, "index.html", content)
+	return render(request, "index.html", {
+        'featured': featured,
+        'recently': recently,
+        'popular': popular,
+        'departments': fetch_department_list()
+    })
