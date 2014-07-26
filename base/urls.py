4e6e36298f0f430c3567df6948beaed61c201ffc
Jarvys <yuhan534@126.com>
2014-07-26 20:06:11 +0800

    save

--- base/urls.py
+++ base/urls.py
@@ -12,4 +12,5 @@ urlpatterns = patterns('',
 
     #url(r'^admin/', include(admin.site.urls)),
     url(r'^index$', portal.views.index),
+    url(r'^video/(?P<video_id>\d+)$', portal.views.video),
 )
--- portal/views.py
+++ portal/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from query import *
+
 
 def index(request):
 	return render(request, "index.html", {
 		'featured': fetch_featured_media(), 
 		'recently': fetch_recently_media(), 
 		'popular': fetch_popular_media(), 
-		'departments': fetch_department_list()['departments'],
+	'departments': fetch_department_list()['departments'],
 		'ads': fetch_ad_list()
     })
+
+
+def video(request, video_id):
+    return redirect("/index")
