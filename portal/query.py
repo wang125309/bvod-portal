6c47e6cff541d0bea71616cd8d1c4c34b6f49a35
PopPopRet <lance.through@the.heart>
2014-07-26 19:44:30 +0800

    接口调整

--- portal/query.py
+++ portal/query.py
@@ -14,15 +14,15 @@ def fetch_featured_media(limit=7):
    featured = {'type':'video', 'featured':'true', 'limit':limit}
    return fetch_query_list(api_url, featured)
 
-def fetch_popular_media(limit=5):
+def fetch_recently_media(limit=5):
+   api_url = base_url+'media/latest'
+   recently = {'limit':limit}
+   return fetch_query_list(api_url, recently)
+
+def fetch_popular_media(limit=7):
    api_url = base_url+'media/popular'
    popular = {'limit':limit}
    return fetch_query_list(api_url, popular)
-
-def fetch_recently_media(limit=7):
-   api_url = base_url+'media'
-   recently = {'type':'video', 'limit':limit}
-   return fetch_query_list(api_url, recently)
 
 def fetch_category_tree(depth=1):
    api_url = base_url+'categories/tree'
@@ -31,12 +31,14 @@ def fetch_category_tree(depth=1):
 
 def fetch_department_list(limit = 7):
    api_url = base_url+'media/departments'
-   department = {}
+   department = {'limit':limit}
    return fetch_query_list(api_url, department, 'departments')
 
 def fetch_ad_list(limit = 7):
-   ads = {'count': 3*limit, 'departments': [{'id': 1, 'seq': i, 'title': 'OOOOO', 'desc': 'sodfasofnjaofasfddddddddas asdfa sdfasdfasdfasdf asdgfl as gfasdfa dd', 'image': 'images/ad_s.jpg', 'url': 'www.baidu.c'} for i in range(limit)]}
-   return ads
+   api_url = base_url+'media/ads'
+   ad = {}
+   ads = fetch_query_list(api_url, ad, 'ads')
+   return {'count':ads['count'], 'ads':ads['ads'][:limit]}
 
 if __name__ == '__main__':
-	print fetch_featured_media()
+	print fetch_ad_list(1)
