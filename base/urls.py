8b91059df8abe47d4ec44b19128cfaa2f13e0564
Jarvys <yuhan534@126.com>
2014-07-27 14:28:39 +0800

    refine navbar

--- base/urls.py
+++ base/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     #url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', portal.views.index),
     url(r'^index$', portal.views.index),
     url(r'^department/(?P<department_id>\d+)$', portal.views.department),
     url(r'^video/(?P<video_id>\d+)$', portal.views.video_detail)
