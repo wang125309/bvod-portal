4d4ce3788ff6d3725143fc8ee44e08357f6a3368
PopPopRet <lance.through@the.heart>
2014-07-28 15:14:13 +0800

    refine api

--- portal/query.py
+++ portal/query.py
@@ -66,7 +66,7 @@ def fetch_ad_list(limit=7):
 # http://bvod.limijiaoyin.com/api/media/get
 def fetch_media_item(id=0):
    r = requests.get(base_url + "media/get", params={'id':id})
-   return r.json() if r.status_code == 200 else None
+   return r.json() if r.status_code == 200 else {}
 
 # http://bvod.limijiaoyin.com/api/media/department_media
 def fetch_deparment_media(id=0, limit=7):
@@ -92,7 +92,65 @@ def fetch_related_media(id=1,limit=3):
 
 # ------------------------
 def fake_department(offset=0, limit=7):
-   s = {"count": 8, "departments": [{"desc": "mmmkkkdfdf", "id": 11, "name": "jiji", "avatar": "11.jpg", "boss": 'aaaa', "video_count":32, "play_count":23, "videos": [{"count": 1, "media": [{"description": null, "views": 0, "likes": 0, "podcast": null, "id": 256, "categories": {}, "author": "admin", "url": "http://bvod.limijiaoyin.com/media/54c9f6b9de087e480a90f234a0a5173e_720flv-41", "title": "54c9f6b9de087e480a90f234a0a5173e_720.flv", "publish_on": "2014-07-27 20:49:12", "slug": "54c9f6b9de087e480a90f234a0a5173e_720flv-41", "comment_count": 0, "urls": [{"url": "http://bvod.limijiaoyin.com/data/media/51ec3007db8bfbacd2918b637f4dd087_720.mp4", "width": 1280, "quality": "720", "height": 720}, {"url": "http://bvod.limijiaoyin.com/data/media/51ec3007db8bfbacd2918b637f4dd087_360.mp4", "width": 480, "quality": "360", "height": 360}], "description_plain": "", "type": "video", "thumbs": {"s": {"url": "http://bvod.limijiaoyin.com/images/media/256s.jpg", "x": 210, "y": 135}, "m": {"url": "http://bvod.limijiaoyin.com/images/media/256m.jpg", "x": 234, "y": 400}, "l": {"url": "http://bvod.limijiaoyin.com/images/media/256l.jpg", "x": 444, "y": 377}}}]}]},{"desc": "mmmkkkdfdf", "id": 11, "name": "jiji", "avatar": "11.jpg", "boss": 'aaaa', "video_count":32, "play_count":23, "videos": [{"count": 1, "media": [{"description": null, "views": 0, "likes": 0, "podcast": null, "id": 256, "categories": {}, "author": "admin", "url": "http://bvod.limijiaoyin.com/media/54c9f6b9de087e480a90f234a0a5173e_720flv-41", "title": "54c9f6b9de087e480a90f234a0a5173e_720.flv", "publish_on": "2014-07-27 20:49:12", "slug": "54c9f6b9de087e480a90f234a0a5173e_720flv-41", "comment_count": 0, "urls": [{"url": "http://bvod.limijiaoyin.com/data/media/51ec3007db8bfbacd2918b637f4dd087_720.mp4", "width": 1280, "quality": "720", "height": 720}, {"url": "http://bvod.limijiaoyin.com/data/media/51ec3007db8bfbacd2918b637f4dd087_360.mp4", "width": 480, "quality": "360", "height": 360}], "description_plain": "", "type": "video", "thumbs": {"s": {"url": "http://bvod.limijiaoyin.com/images/media/256s.jpg", "x": 210, "y": 135}, "m": {"url": "http://bvod.limijiaoyin.com/images/media/256m.jpg", "x": 234, "y": 400}, "l": {"url": "http://bvod.limijiaoyin.com/images/media/256l.jpg", "x": 444, "y": 377}}}]}]},{"desc": "mmmkkkdfdf", "id": 11, "name": "jiji", "avatar": "11.jpg", "boss": 'aaaa', "video_count":32, "play_count":23, "videos": [{"count": 1, "media": [{"description": null, "views": 0, "likes": 0, "podcast": null, "id": 256, "categories": {}, "author": "admin", "url": "http://bvod.limijiaoyin.com/media/54c9f6b9de087e480a90f234a0a5173e_720flv-41", "title": "54c9f6b9de087e480a90f234a0a5173e_720.flv", "publish_on": "2014-07-27 20:49:12", "slug": "54c9f6b9de087e480a90f234a0a5173e_720flv-41", "comment_count": 0, "urls": [{"url": "http://bvod.limijiaoyin.com/data/media/51ec3007db8bfbacd2918b637f4dd087_720.mp4", "width": 1280, "quality": "720", "height": 720}, {"url": "http://bvod.limijiaoyin.com/data/media/51ec3007db8bfbacd2918b637f4dd087_360.mp4", "width": 480, "quality": "360", "height": 360}], "description_plain": "", "type": "video", "thumbs": {"s": {"url": "http://bvod.limijiaoyin.com/images/media/256s.jpg", "x": 210, "y": 135}, "m": {"url": "http://bvod.limijiaoyin.com/images/media/256m.jpg", "x": 234, "y": 400}, "l": {"url": "http://bvod.limijiaoyin.com/images/media/256l.jpg", "x": 444, "y": 377}}}]}]} ]}
+   s = {
+         "count": 8,
+         "departments": [{
+            "desc": "mmmkkkdfdf",
+            "id": 11,
+            "name": "jiji",
+            "avatar": "11.jpg",
+            "boss": "sdfasfas",
+            "video_count": 32,
+            "play_count": 23,
+            "videos": [{
+               "count": 1,
+               "media": [{
+                  "description": null,
+                  "views": 0,
+                  "likes": 0,
+                  "podcast": null,
+                  "id": 256,
+                  "categories": {},
+                  "author": "admin",
+                  "url": "http://bvod.limijiaoyin.com/media/54c9f6b9de087e480a90f234a0a5173e_720flv-41",
+                  "title": "54c9f6b9de087e480a90f234a0a5173e_720.flv",
+                  "publish_on": "2014-07-27 20:49:12",
+                  "slug": "54c9f6b9de087e480a90f234a0a5173e_720flv-41",
+                  "comment_count": 0,
+                  "urls": [{
+                     "url": "http://bvod.limijiaoyin.com/data/media/51ec3007db8bfbacd2918b637f4dd087_720.mp4",
+                     "width": 1280,
+                     "quality": "720",
+                     "height": 720
+                  }, {
+                     "url": "http://bvod.limijiaoyin.com/data/media/51ec3007db8bfbacd2918b637f4dd087_360.mp4",
+                     "width": 480,
+                     "quality": "360",
+                     "height": 360
+                  }],
+                  "description_plain": "",
+                  "type": "video",
+                  "thumbs": {
+                     "s": {
+                        "url": "http://bvod.limijiaoyin.com/images/media/256s.jpg",
+                        "x": 210,
+                        "y": 135
+                     },
+                     "m": {
+                        "url": "http://bvod.limijiaoyin.com/images/media/256m.jpg",
+                        "x": 234,
+                        "y": 400
+                     },
+                     "l": {
+                        "url": "http://bvod.limijiaoyin.com/images/media/256l.jpg",
+                        "x": 444,
+                        "y": 377
+                     }
+                  }
+               }]
+            }]
+         }]
+      }
    return s
 
 # http://bvod.limijiaoyin.com/api/media/latest
